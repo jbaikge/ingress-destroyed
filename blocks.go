dbd01a9ddb88e407335552bba54359bdc9458152
jake <bigjake2s@gmail.com>
2013-05-23 23:50:09 -0400

    Implemented mboxMessageBlocks and test

--- blocks.go
+++ blocks.go
@@ -1,15 +1,38 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"io"
 )
 
 // Reads an mbox-formatted file and returns the message blocks. The format
 // dictates that each message begins with a "From " line
-func mboxMessageBlocks(r io.Reader) (msgs chan<- []byte, err error) {
-	msgs = make(chan []byte, 1)
-	buf := bytes.NewBuffer(make([]byte, 4096))
-	msgs <- buf.Bytes()
-	return
+func mboxMessageBlocks(r io.Reader, blocks chan []byte) {
+	defer close(blocks)
+
+	br := bufio.NewReader(r)
+	buf := bytes.NewBuffer(make([]byte, 0, 4096))
+
+	for {
+		line, err := br.ReadBytes('\n')
+		if err != nil && err != io.EOF {
+			break
+		}
+
+		// Send buffered message when encountering a new message
+		if len(line) > 5 && string(line[0:5]) == "From " && buf.Len() > 0 {
+			block := make([]byte, buf.Len())
+			copy(block, buf.Bytes())
+			blocks <- block
+			buf.Reset()
+		}
+		buf.Write(line)
+
+		// Send last buffered message
+		if err == io.EOF {
+			blocks <- buf.Bytes()
+			break
+		}
+	}
 }
--- blocks_test.go
+++ blocks_test.go
@@ -7,23 +7,28 @@ import (
 
 func TestSimpleBlocks(t *testing.T) {
 	lines := [][]byte{
-		[]byte("From joe@test.com Thu May 23 22:00:04 2013"),
-		[]byte("From joe@test.com Thu May 23 22:00:04 2013"),
-		[]byte("From joe@test.com Thu May 23 22:00:05 2013"),
-		[]byte("From joe@test.com Thu May 23 22:00:06 2013"),
-		[]byte("From joe@test.com Thu May 23 22:00:07 2013"),
-		[]byte("From joe@test.com Thu May 23 22:00:07 2013"),
-		[]byte("From joe@test.com Thu May 23 22:00:08 2013"),
-		[]byte("From joe@test.com Thu May 23 22:00:08 2013"),
-		[]byte("From joe@test.com Thu May 23 22:00:09 2013"),
-		[]byte("From joe@test.com Thu May 23 22:00:10 2013"),
+		[]byte("From joe01@test.com Thu May 23 22:00:04 2013\n"),
+		[]byte("From joe02@test.com Thu May 23 22:00:04 2013\n"),
+		[]byte("From joe03@test.com Thu May 23 22:00:05 2013\n"),
+		[]byte("From joe04@test.com Thu May 23 22:00:06 2013\n"),
+		[]byte("From joe05@test.com Thu May 23 22:00:07 2013\n"),
+		[]byte("From joe06@test.com Thu May 23 22:00:07 2013\n"),
+		[]byte("From joe07@test.com Thu May 23 22:00:08 2013\n"),
+		[]byte("From joe08@test.com Thu May 23 22:00:08 2013\n"),
+		[]byte("From joe09@test.com Thu May 23 22:00:09 2013\n"),
+		[]byte("From joe10@test.com Thu May 23 22:00:10 2013\n"),
 	}
-	r := bytes.NewReader(bytes.Join(lines, []byte("\n\n")))
-	msgs, err := mboxMessageBlocks(r)
-	if err != nil {
-		t.Fatal(err)
-	}
-	for msg := range msgs {
-		t.Log(string(msg))
+	r := bytes.NewReader(bytes.Join(lines, []byte{}))
+
+	blocks := make(chan []byte, 0)
+	go mboxMessageBlocks(r, blocks)
+
+	i := 0
+	for block := range blocks {
+		exp := string(lines[i])
+		if exp != string(block) {
+			t.Errorf("Incorrect block.\nExpect: '%s'\nGot: '%s'", exp, string(block))
+		}
+		i++
 	}
 }
