90af7f853156e0d06d3330c5056b75b2909094f9
jake <bigjake2s@gmail.com>
2013-05-23 23:50:26 -0400

    Implemented simple main func to flex blocker

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"log"
+	"os"
+)
 
+var (
+	mboxFilename = flag.String("f", "/var/mail/jake", "Path to mbox file to parse")
+)
+
+func main() {
+	flag.Parse()
+
+	f, err := os.Open(*mboxFilename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	msgs := make(chan []byte)
+	go mboxMessageBlocks(f, msgs)
+	for msg := range msgs {
+		log.Print(string(msg))
+	}
 }
