5b41585f1a539b6e12c87624d586a1a47a7d8e98
jake <bigjake2s@gmail.com>
2013-06-07 17:46:30 -0400

    Added GMap string call

--- location/action.go
+++ location/action.go
@@ -1,6 +1,7 @@
 package location
 
 import (
+	"fmt"
 	"time"
 )
 
@@ -10,4 +11,8 @@ type Action struct {
 	Time      time.Time
 	Count     int
 	Location  Point
+}
+
+func (a *Action) GMap() string {
+	return fmt.Sprintf("\t\t{location: new google.maps.LatLng(%0.6f, %0.6f), weight: %d},", a.Location.Lat, a.Location.Lon, a.Count)
 }
