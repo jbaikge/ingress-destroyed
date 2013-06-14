781c5612659a03a100d9b6184176ce718d7f564e
jake <bigjake2s@gmail.com>
2013-06-14 07:17:20 -0400

    Configured the config package smarter

--- config/config.go
+++ config/config.go
@@ -2,13 +2,14 @@ package config
 
 import (
 	"flag"
+	"fmt"
 	"io/ioutil"
 	"launchpad.net/goyaml"
 	"log"
 	"os"
 )
 
-type configType struct {
+type config struct {
 	Imap    login
 	Mbox    string
 	Storage storage
@@ -26,29 +27,52 @@ type storage struct {
 	MySQL  login
 }
 
-var Config = configType{}
+var (
+	Imap    = login{}
+	Mbox    = ""
+	Storage = storage{}
+)
 
 func init() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 
-	var configFile = os.Getenv("HOME") + "/.config/ingress-destroyed.yaml"
-	if f, err := os.Open(configFile); err == nil {
-		b, err := ioutil.ReadAll(f)
-		if err != nil {
-			log.Fatal("Error reading", configFile, ":", err)
-		}
-		if err := goyaml.Unmarshal(b, &Config); err != nil {
-			log.Fatal("Error processing", configFile, ":", err)
-		}
+	if err := ReadFromFile(os.Getenv("HOME") + "/.config/ingress-destroyed.yaml"); err != nil {
+		log.Fatalf("Error processing configuration: %s", err)
 	}
 
-	flag.StringVar(&Config.Imap.Host, "imap.host", Config.Imap.Host, "IMAP Host")
-	flag.StringVar(&Config.Imap.Username, "imap.username", Config.Imap.Username, "IMAP Username")
-	flag.StringVar(&Config.Imap.Password, "imap.password", Config.Imap.Password, "IMAP Password")
-	flag.StringVar(&Config.Mbox, "mbox", Config.Mbox, "User mbox path (usually /var/mail/user)")
-	flag.StringVar(&Config.Storage.MySQL.Host, "storage.mysql.host", Config.Storage.MySQL.Host, "MySQL Host")
-	flag.StringVar(&Config.Storage.MySQL.Username, "storage.mysql.username", Config.Storage.MySQL.Username, "MySQL Username")
-	flag.StringVar(&Config.Storage.MySQL.Password, "storage.mysql.password", Config.Storage.MySQL.Password, "MySQL Password")
-	flag.StringVar(&Config.Storage.CSV, "storage.csv", Config.Storage.CSV, "CSV Path")
-	flag.StringVar(&Config.Storage.SQLite, "storage.sqlite", Config.Storage.SQLite, "SQLite Database Path")
+	flag.StringVar(&Imap.Host, "imap.host", Imap.Host, "IMAP Host")
+	flag.StringVar(&Imap.Username, "imap.username", Imap.Username, "IMAP Username")
+	flag.StringVar(&Imap.Password, "imap.password", Imap.Password, "IMAP Password")
+	flag.StringVar(&Mbox, "mbox", Mbox, "User mbox path (usually /var/mail/user)")
+	flag.StringVar(&Storage.MySQL.Host, "storage.mysql.host", Storage.MySQL.Host, "MySQL Host")
+	flag.StringVar(&Storage.MySQL.Username, "storage.mysql.username", Storage.MySQL.Username, "MySQL Username")
+	flag.StringVar(&Storage.MySQL.Password, "storage.mysql.password", Storage.MySQL.Password, "MySQL Password")
+	flag.StringVar(&Storage.CSV, "storage.csv", Storage.CSV, "CSV Path")
+	flag.StringVar(&Storage.SQLite, "storage.sqlite", Storage.SQLite, "SQLite Database Path")
+}
+
+func ReadFromFile(filename string) (err error) {
+	f, err := os.Open(filename)
+	if err != nil {
+		return nil
+	}
+	defer f.Close()
+
+	b, err := ioutil.ReadAll(f)
+	if err != nil {
+		return fmt.Errorf("Error reading %s: %s", filename, err)
+	}
+	c := struct {
+		Imap    *login
+		Mbox    *string
+		Storage *storage
+	}{
+		Imap:    &Imap,
+		Mbox:    &Mbox,
+		Storage: &Storage,
+	}
+	if err := goyaml.Unmarshal(b, &c); err != nil {
+		return fmt.Errorf("Error processing %s: %s", filename, err)
+	}
+	return
 }
