6b9a2eaf020b39d741f3b19004740eb22779c115
jake <bigjake2s@gmail.com>
2013-06-15 22:08:54 -0400

    Built all necessary utilities to pull mail from IMAP

--- imap/imap.go
+++ imap/imap.go
@@ -2,17 +2,23 @@ package imap
 
 import (
 	"code.google.com/p/rsc/imap"
+	"fmt"
+	"github.com/jbaikge/ingress-destroyed/config"
 	"github.com/jbaikge/ingress-destroyed/message"
 	"log"
+	"time"
 )
 
 type Imap struct {
-	Client *imap.Client
-	Inbox  *imap.Box
+	Client  *imap.Client
+	Inbox   *imap.Box
+	oldMsgs map[*imap.Msg]bool
 }
 
 func Login(server, user, passwd string) (i *Imap, err error) {
-	i = &Imap{}
+	i = &Imap{
+		oldMsgs: make(map[*imap.Msg]bool),
+	}
 	i.Client, err = imap.NewClient(imap.Unencrypted, server, user, passwd, "")
 	if err != nil {
 		return
@@ -21,10 +27,62 @@ func Login(server, user, passwd string) (i *Imap, err error) {
 	return
 }
 
-func (i *Imap) Messages(msgs chan *message.Message) {
+func (c *Imap) LiveMessages(msgChan chan *imap.Msg) {
 	log.Println("Starting IMAP Message poll")
-	inbox := i.Client.Inbox()
-	if err := inbox.Check(); err != nil {
-		log.Printf("Error checking inbox: %s", err)
+	for {
+		log.Print("Polling...")
+
+		<-time.After(config.Imap.Refresh)
 	}
+}
+
+func (c *Imap) AllMessages() (msgs []*imap.Msg, err error) {
+	log.Println("Checking for new messages...")
+	if err = c.Inbox.Check(); err != nil {
+		err = fmt.Errorf("Error checking inbox: %s", err)
+		return
+	}
+	msgs = c.Inbox.Msgs()
+	log.Printf("Inbox contains %d messages", len(msgs))
+	return
+}
+
+func (c *Imap) NewMessages() (msgs []*imap.Msg, err error) {
+	all, err := c.AllMessages()
+	if err != nil {
+		return
+	}
+	for _, m := range all {
+		if _, ok := c.oldMsgs[m]; ok {
+			m.Flags |= imap.FlagDeleted
+		} else {
+			msgs = append(msgs, m)
+		}
+		c.oldMsgs[m] = false
+	}
+	for i := range c.oldMsgs {
+		if c.oldMsgs[i] {
+			delete(c.oldMsgs, i)
+		} else {
+			c.oldMsgs[i] = true
+		}
+	}
+	log.Printf("%d new message(s)", len(msgs))
+	return
+}
+
+func ToMessage(msg *imap.Msg) (m *message.Message) {
+	m = &message.Message{
+		Id:   fmt.Sprint(msg.UID),
+		Date: msg.Date,
+	}
+	for _, c := range msg.Root.Child {
+		switch c.Type {
+		case "text/html":
+			m.HTML = c.Text()
+		case "text/plain":
+			m.Text = c.Text()
+		}
+	}
+	return
 }
--- imap/imap_test.go
+++ imap/imap_test.go
@@ -13,5 +13,65 @@ func TestLogin(t *testing.T) {
 	if err != nil {
 		t.Fatalf("Could not login: %s", err)
 	}
+	defer c.Client.Close()
 	t.Logf("Inbox name: %s", c.Inbox.Name)
+}
+
+func TestAllMessages(t *testing.T) {
+	if testing.Short() {
+		t.Skip("Short tests enabled")
+	}
+	c, err := Login(config.Imap.Host, config.Imap.Username, config.Imap.Password)
+	if err != nil {
+		t.Fatalf("Could not login: %s", err)
+	}
+	defer c.Client.Close()
+	msgs, err := c.AllMessages()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Logf("%d messages", len(msgs))
+}
+
+func TestNewMessages(t *testing.T) {
+	if testing.Short() {
+		t.Skip("Short tests enabled")
+	}
+	c, err := Login(config.Imap.Host, config.Imap.Username, config.Imap.Password)
+	if err != nil {
+		t.Fatalf("Could not login: %s", err)
+	}
+	defer c.Client.Close()
+	msgs0, err := c.NewMessages()
+	if err != nil {
+		t.Fatal(err)
+	}
+	msgs1, err := c.NewMessages()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if l := len(msgs0); l == 0 {
+		t.Fatalf("Got zero messages for first call")
+	}
+	if l := len(msgs1); l != 0 {
+		t.Fatalf("Second call should be zero; got: %d", l)
+	}
+}
+
+func TestSingleMessage(t *testing.T) {
+	if testing.Short() {
+		t.Skip("Short tests enabled")
+	}
+	c, err := Login(config.Imap.Host, config.Imap.Username, config.Imap.Password)
+	if err != nil {
+		t.Fatalf("Could not login: %s", err)
+	}
+	defer c.Client.Close()
+	msgs, err := c.AllMessages()
+	if err != nil {
+		t.Fatal(err)
+	}
+	m := ToMessage(msgs[0])
+	t.Logf("ID: %s\nDate: %s\nHTML len: %d\nText len %d", m.Id, m.Date, len(m.HTML), len(m.Text))
 }
